5a2a0b2d44f772f861f1bcd570317aadfc64226d
A.I <merrick@luois.me>
2017-08-11 22:24:05 +0800

    init

--- src/window.cc
+++ src/window.cc
@@ -5,15 +5,13 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char * args[]) {
-  SDL_Window* window = NULL;
-	SDL_Surface* surface = NULL;
+SDL_Window* create_window() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		printf("SDL could not initialze, SDL_Error: %s\n", SDL_GetError());
-		return -1;
+		return NULL;
 	}
 
-	window = SDL_CreateWindow("The Red Baron",
+	auto window = SDL_CreateWindow("The Red Baron",
 														SDL_WINDOWPOS_UNDEFINED,
 														SDL_WINDOWPOS_UNDEFINED,
 														SCREEN_WIDTH,
@@ -21,10 +19,14 @@ int main(int argc, char * args[]) {
 														SDL_WINDOW_SHOWN);
 	if (window == NULL) {
 		printf("Cannot create window, SDL_Error: %s\n", SDL_GetError());
-		return -2;
+		return NULL;
 	}
+	return window;
+}
 
-	surface = SDL_GetWindowSurface(window);
+int main(int argc, char * args[]) {
+	auto window = create_window();
+	auto surface = SDL_GetWindowSurface(window);
 	SDL_FillRect(surface,
 							 NULL,
 							 SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
